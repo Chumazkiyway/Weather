d4b6db48c3beee88bf8c1b4af4a179da5597690c
Bogdan Chumak <bogdan.ch@it-dimension.com>
2019-09-25 14:27:26 +0300

    toolbar was added

--- app/src/main/java/com/chumazkiyway/weather/MainActivity.kt
+++ app/src/main/java/com/chumazkiyway/weather/MainActivity.kt
@@ -2,11 +2,25 @@ package com.chumazkiyway.weather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_items, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
